c040d54dddca1fa50b750849b0cf73bde4b2ef4e
Habib7442 <habibtanwir1906@gmail.com>
2024-12-09 17:36:14 +0530

    added footer

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,11 +7,11 @@ const Logo = () => {
     <Link href="/" className="flex items-center space-x-2">
       <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
         <Image
-          src="/logo-facesearch.jpeg"
+          src="/logo-facesearch.png"
           alt="FaceSearch AI Logo"
           width={40} // Set a specific width
           height={40} // Set a specific height
-          className="object-contain rounded-2xl"
+          className="object-contain rounded-md"
         />
       </motion.div>
       {/* <span className="text-ml font-bold bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-slate-900">
